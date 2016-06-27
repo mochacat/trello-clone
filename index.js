215840976fc711de5f52ed87f7312b51d483f44b
James Lee <uptownhr@gmail.com>
2016-06-27 13:17:59 -0700

    moved client to /src directory. implemented .kuberc with kube. added kube middleware to index.js

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 const app = require('./bootstrap'),
   controllers = require('./controllers'),
   models = require('./models'),
-  passport = require('./config/passport')
+  passport = require('./config/passport'),
+  kube = require('kube')
 
-app.use('/', controllers.Home)
+kube(app)
+
+app.get('/', function(req,res){
+  res.kube.render()
+})
+
+
+/*app.use('/', controllers.Home)
 app.use('/board', controllers.Board)
 app.use('/blog', controllers.Post)
 app.use('/auth', controllers.Auth)
 app.use('/user', passport.isAuthenticated, controllers.User)
-app.use('/admin', passport.isAuthenticated, passport.isAdmin, controllers.Admin)
+app.use('/admin', passport.isAuthenticated, passport.isAdmin, controllers.Admin)*/
 
 module.exports = app
